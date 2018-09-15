07c9f064a6426f7deb308ba6ecedc5e3c6b62eb0
Hano <hinsul3113@gmail.com>
2018-09-15 02:31:56 -0700

    API endpoint for getting all questions and deleting a question with all related answers

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -2,6 +2,21 @@ const express = require('express');
       router  = express.Router();
       knex    = require('../db/knex');
 
+// get all questions from questions table and their relative answers
+// localhost:3000/questions
+router.get('/', (req, res, next) => {
+  // KNEX WAY
+  // knex.from('questions').innerJoin('answers', 'questions.id', 'answers.question_id')
+  //     .then(questions => {
+  //       res.send(questions);
+  //     });
+
+  // Raw SQL
+  knex.raw('select * from questions t1, answers t2 where t1.id = t2.question_id').then(questions => {
+    res.send({ questions: questions.rows })
+  });
+});
+
 // get a question and its related answers endpoint
 // localhost:3000/questions/:id
 router.get('/:id', (req, res, next) => {
@@ -16,5 +31,20 @@ router.get('/:id', (req, res, next) => {
               });
         })
 });
+
+// delete a question and its related answers endpoint
+// localhost:3000/questions/:id
+router.delete('/:id', (req, res, next) => {
+  let { id } = req.params;
+  knex('answers').where('question_id', id).del()
+    .then(() => {
+      knex('questions').where('id', id).del()
+        .then(() => {
+          knex.select().from('questions').then(question => {
+            res.send(question)
+          })
+        })
+    })
+})
 
 module.exports = router;

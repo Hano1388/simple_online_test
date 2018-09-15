70afa8f75208645f1306000a98822cc4c593a38d
Hano <hinsul3113@gmail.com>
2018-09-15 14:06:12 -0700

    created enpointed to get a single question

--- server/routes/answers.js
+++ server/routes/answers.js
@@ -2,10 +2,20 @@ const express = require('express');
       router  = express.Router();
       knex    = require('../db/knex');
 
+// get an answer for updating and deleting
+// localhost:3000/answers/:id
+router.get('/:id', (req, res) => {
+  let { id } = req.params;
+
+  knex.select().from('answers').where('id', id)
+      .then(answer => {
+        res.send(answer);
+      });
+});
 
 // create answers for the question
-// localhost:3000/answer/
-router.post('/', (req, res, next) => {
+// localhost:3000/answers/
+router.post('/', (req, res) => {
   // console.log('request object body: ', req.body);
   knex.raw('insert into answers(answer, is_correct_answer, question_id) values(?, ?, ?)', ['spiders', true, 11])
   .then(() => {
@@ -15,6 +25,7 @@ router.post('/', (req, res, next) => {
     })
   })
 });
+
 
 
 module.exports = router;
